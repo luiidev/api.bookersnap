3391681abf10617a80b269ad5e589ab2049ca7ae
marxquva <marxquva@gmail.com>
2016-09-12 15:13:59 -0500

    update and cambio de estado reservacion

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -4,10 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\Http\Requests;
 use App\Http\Requests\ReservationRequest;
 use App\Http\Controllers\Controller as Controller;
 use App\Services\ReservationService;
+use Illuminate\Support\Facades\Input;
 use Carbon\Carbon;
 
 class ReservationController extends Controller
@@ -37,6 +37,26 @@ class ReservationController extends Controller
                     return response()->json($result);
                 });
                 
+    }
+
+    public function update(ReservationRequest $request) {
+    	//$service = $this->_ReservationService;
+    	$microsite_id = $request->route('microsite_id');
+        $reservation_id = $request->route('reservation_id');
+        
+        
+        return $this->TryCatch(function () use ($request, $microsite_id, $reservation_id) {
+    		$result = $this->_ReservationService->update($request->all(), $microsite_id, $reservation_id, $request->_bs_user_id);
+            return response()->json($result);
+        });
+    }
+
+    public function delete(Request $request) {
+        $service = $this->_ReservationService;
+        return $this->TryCatch(function () use ($request, $service) {
+        	$result = $service->delete($request->route('microsite_id'), $request->route('reservation_id'));
+            return response()->json($result);
+        });
     }
 
 }
