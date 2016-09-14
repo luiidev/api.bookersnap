05995cc1fecc9435f01cf20959c0446fc7e5dc21
obedtrejolopez <obedtrejolopez@gmail.com>
2016-09-14 10:30:40 -0500

    Se agrega una libreria de funciones comunes

--- app/Helpers/Utilitarios.php
+++ app/Helpers/Utilitarios.php
@@ -77,7 +77,6 @@ class Utilitarios {
       $minutes  = floor($seconds/60);
       $seconds -= $minutes*60;
       return "{$hours}:{$minutes}:{$seconds}";
-      //return sprintf('%02d:%02d:%02d', $hours, $minutes, $seconds); 
     }
 
 }
