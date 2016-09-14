18b9b965c6aca724d702ff62cc9903e3c098a0b8
obedtrejolopez <obedtrejolopez@gmail.com>
2016-09-14 16:23:03 -0500

    Api de servidores terminado

---
+++ app/Http/Requests/ServerCreateRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class ServerCreateRequest extends Request {
+
+    public function wantsJson() {
+        return true;
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize() {
+        return true;
+    }
+
+    public function formatErrors(\Illuminate\Contracts\Validation\Validator $validator) {
+        $data["response"] = false;
+        $data["jsonError"] = $validator->errors()->all();
+        return $data;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules() {
+        return [
+            'name' => 'required',
+            'color' => 'required',
+            'tables' => 'required|array',
+            'tables.*.id' => 'required|int',
+        ];
+    }
+
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -52,10 +52,21 @@ function routeMesas()
         Route::get('blocks/tables', 'BlockController@getTables');
         Route::get('blocks/{block_id}', 'BlockController@getBlock');
         Route::put('blocks/{block_id}', 'BlockController@update');
-        //Route::get('zones/{zone_id}/tables', 'ZoneController@listTable');
-        //Route::post('zones/', 'ZoneController@create');        
-        //Route::put('zones/{zone_id}', 'ZoneController@update');
-        //Route::delete('zones/{zone_id}', 'ZoneController@delete');
+
+        //-----------------------------------------------------
+        // MICROSITE::SERVERS
+        //-----------------------------------------------------
+        Route::get('servers', 'ServerController@listado');
+        Route::post('servers', 'ServerController@insert');
+        Route::put('servers/{server_id}', 'ServerController@update');
+        Route::delete('servers/{server_id}', 'ServerController@delete');
+        /*
+        Route::post('blocks', 'BlockController@insert');
+        Route::get('blocks', 'BlockController@list');
+        Route::get('blocks/tables', 'BlockController@getTables');
+        Route::get('blocks/{block_id}', 'BlockController@getBlock');
+        Route::put('blocks/{block_id}', 'BlockController@update');
+        */
 
         //-----------------------------------------------------
         // MICROSITE::TURNOS
