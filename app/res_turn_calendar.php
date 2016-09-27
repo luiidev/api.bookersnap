bdb010866823f51bc31071c42bde7ab16abc5424
Luis Vasquez <luizhito.lp.4ever@gmail.com>
2016-09-27 17:12:25 -0500

    Se agrego dayOfWeek como entero a la varialbe  al modelo res_turn_calendar

--- app/res_turn_calendar.php
+++ app/res_turn_calendar.php
@@ -19,8 +19,9 @@ class res_turn_calendar extends Model {
 
     protected $table = "res_turn_calendar";
     public $timestamps = false;
-   protected $fillable = ['res_type_turn_id', 'start_date', 'end_date', 'start_time', 'end_time', 'date_add', 'user_add', 'res_turn_id'];
-   // protected $hidden = ['ms_microsite_id', 'ev_event_id', 'bs_user_id'];
+    protected $fillable = ['res_type_turn_id', 'start_date', 'end_date', 'start_time', 'end_time', 'date_add', 'user_add', 'res_turn_id'];
+    // protected $hidden = ['ms_microsite_id', 'ev_event_id', 'bs_user_id'];
+    protected $casts = ["dayOfWeek" => "integer"];
     
     public function turn() {
         return $this->belongsTo('App\res_turn', 'res_turn_id');
