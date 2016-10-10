e3853526542e1f73665808022c2cd81c4e604714
Luis Vasquez <luizhito.lp.4ever@gmail.com>
2016-10-10 12:44:36 -0500

    Se creo ruta, validacion, servicio y controlador para creacion de reservacion de mesas

---
+++ app/Services/TableReservationService.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Services;
+
+use App\res_guest;
+use App\res_guest_email;
+use App\res_guest_phone;
+use App\res_reservation;
+use DB;
+
+class TableReservationService
+{
+    private $lang;
+    private $microsite_id;
+    private $guest;
+
+    public function __construct(string $lang, int $microsite_id)
+    {
+        $this->lang = $lang;
+        $this->microsite_id = $microsite_id;
+    }
+
+    public static function make($lang, $microsite_id) {
+        return new static((string)$lang, (int)$microsite_id);
+    }
+
+    public function find_guest()
+    {
+        $this->guest = res_guest::find(request("guest_id"));
+    }
+
+    public function create_guest()
+    {
+        $guest = new res_guest();
+        $guest->first_name = request("guest.first_name");
+        $guest->last_name = request("guest.last_name");
+        $guest->user_add = request("_bs_user_id");
+        $guest->ms_microsite_id = $this->microsite_id;
+
+        $guest->save();
+
+        $this->guest = $guest;
+    }
+
+    public function create_guest_email()
+    {
+        $guest_email = new res_guest_email();
+        $guest_email->email = request("guest.email");
+
+        $this->guest->emails()->save($guest_email);
+    }
+
+    public function create_guest_phone()
+    {
+        $guest_phone = new res_guest_phone();
+        $guest_phone->number = request("guest.phone");
+
+        $this->guest->phones()->save($guest_phone);
+    }
+
+    public function create_reservation()
+    {
+        $email = $phone = null;
+        if (isset($this->guest->emails->first()->number)) $phone = $this->guest->emails->first()->number;
+        if (isset($this->guest->phones->first()->number)) $phone = $this->guest->phones->first()->number;
+        $reservation = new res_reservation();
+        $reservation->res_guest_id = isset($this->guest)?$this->guest->id:null;
+        $reservation->status_released = request("status_id");
+        $reservation->num_people = request("covers");
+        $reservation->date_reservation = request("date");
+        $reservation->hours_reservation = request("hour");
+        $reservation->hours_duration = request("duration");
+        // $reservation->res_server_id = request("server_id");
+        $reservation->note = request("note");
+        $reservation->phone = $phone;
+        $reservation->email = $email;
+        $reservation->user_add = request("_bs_user_id");
+        $reservation->ms_microsite_id = $this->microsite_id;
+
+        $reservation->save();
+    }
+}
---
+++ app/Services/Traits/ResponseFormatTrait.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Services\Traits;
+
+trait ResponseFormatTrait
+{
+     protected function CreateJsonResponse($success, $statusCode, $msg = null, $data = null, $redirect = false, $url = null, $errorUserMsg = null, $errorInternalMsg = null, $arrayErrors = null) {
+        $response = [
+            "success" => $success,
+            "statuscode" => $statusCode,
+            "msg" => $msg,
+            "data" => $data,
+            "redirect" => $redirect,
+            "url" => $url,
+            "error" => [
+                "user_msg" => $errorUserMsg,
+                "internal_msg" => $errorInternalMsg,
+                "errors" => $arrayErrors
+            ]
+        ];
+
+        return response()->json($response, $statusCode);
+    }
+}
