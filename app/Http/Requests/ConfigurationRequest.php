7fa7cebdaa6e9c48beefb42a842c92f341166f2b
Cesar Moreno <stonblizzard@gmail.com>
2016-10-11 18:00:44 -0500

    Se creo archivo Configuration Request

---
+++ app/Http/Requests/ConfigurationRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class ConfigurationRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "name" => "required|string|max:255",
+        ];
+    }
+
+    public function response(array $errors)
+    {
+        return $this->CreateJsonResponse(false, 422, "", $errors, null, null, "Parametros recibidos no son validos");
+    }
+}
