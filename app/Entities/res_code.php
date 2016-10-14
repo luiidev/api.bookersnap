a0d5a4c5bb88a6280e260b53a32999d554b62552
Cesar Moreno <stonblizzard@gmail.com>
2016-10-14 12:33:16 -0500

    Se configuro el modelo res_code

--- app/Entities/res_code.php
+++ app/Entities/res_code.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class res_code extends Model
 {
-    //
+    protected $table   = "res_code";
+    protected $id      = "ms_microsite_id";
+    public $timestamps = false;
+
 }
