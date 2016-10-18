408e47acc6404c46cbdef2eb7bce700fedf2823d
joper30 <joper30@gmail.com>
2016-10-17 19:25:38 -0500

    updateNotesok7

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -18,7 +18,7 @@ class NoteController extends Controller
     public function index(Request $request)
     {
         return $this->TryCatchDB(function () use ($request) {
-            $date = (isset($request->input('date')) ? $request->input('date') : date('Y-m-d'));
+            $date = ($request->has('date')) ? $request->input('date') : date('Y-m-d');
             $note = $this->_NoteService->getList($request->route('microsite_id'), $date);
             return $this->CreateJsonResponse(true, 201, "Listado de notas", $note);
         });
