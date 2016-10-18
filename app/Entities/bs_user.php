be9650426adc97513a89909c3976f33555e02057
Cesar Moreno <stonblizzard@gmail.com>
2016-10-18 11:11:56 -0500

    Se completo relacion de microsite, user, login & socialnetwork

--- app/Entities/bs_user.php
+++ app/Entities/bs_user.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class bs_user extends Model
 {
-    //
+    protected $table = "bs_user";
+
+    public function logins()
+    {
+        return $this->hasMany('App\Entities\bs_userlogin', 'bs_user_id', 'id');
+    }
+
+    public function privileges()
+    {
+        return $this->belongsToMany('App\Entities\ms_microsite', 'res_privilege', 'bs_user_id', 'ms_microsite_id')->withPivot('date_add', 'user_add');
+    }
+
+    public function microsites()
+    {
+        return $this->hasMany('App\Entities\ms_microsite', 'bs_user_id', 'id');
+    }
+
+    public function socials()
+    {
+        return $this->hasManythrough('App\Entities\bs_socialnetwork', 'App\Entities\bs_userlogin', 'bs_user_id', 'bs_socialnetwork_id', 'id');
+    }
+
 }
