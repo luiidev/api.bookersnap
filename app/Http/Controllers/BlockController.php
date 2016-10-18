f1ff79bde83b1c549191cbf0403b118cdaa6449b
joper30 <joper30@gmail.com>
2016-10-18 10:14:15 -0500

    routes

--- app/Http/Controllers/BlockController.php
+++ app/Http/Controllers/BlockController.php
@@ -1,73 +1,79 @@
 <?php
 
-
 namespace App\Http\Controllers;
-use App\Services\BlockService;
+
+use App\Http\Controllers\Controller as Controller;
 use App\Http\Requests\BlockCreateRequest;
 use App\Http\Requests\BlockListRequest;
 use App\Http\Requests\BlockUpdateRequest;
-use App\Http\Controllers\Controller as Controller;
+use App\Services\BlockService;
+use Illuminate\Http\Request;
 
-class BlockController  extends Controller {
+class BlockController extends Controller
+{
 
     protected $_blockService;
 
-    public function __construct(BlockService $blockService) {
+    public function __construct(BlockService $blockService)
+    {
         $this->_blockService = $blockService;
     }
 
-	public function delete($lang, $microsite, $block_id){
+    public function delete(Request $request)
+    {
 
-		return $this->TryCatch(function () use ($microsite, $block_id) {
-            $data = $this->_blockService->delete($microsite, $block_id);
+        return $this->TryCatch(function () use ($request) {
+            $data = $this->_blockService->delete($request->route('microsite_id'), $request->route('block_id'));
             return $this->CreateJsonResponse($data->estado, 201, trans($data->mensaje));
         });
-	}
+    }
 
-    public function insert($lang, $microsite, BlockCreateRequest $request){
+    public function insert(BlockCreateRequest $request)
+    {
 
-        return $this->TryCatch(function () use ($microsite, $request) {
-            $data = $this->_blockService->insert($microsite, $request->all());
+        return $this->TryCatch(function () use ($request) {
+            $data = $this->_blockService->insert($request->route('microsite_id'), $request->all());
             return $this->CreateJsonResponse($data->estado, 201, trans($data->mensaje));
         });
 
     }
-    
-    public function getBlock($lang, $microsite, $id_block){
 
-        return $this->TryCatch(function () use ($microsite, $id_block) {
-            $data = $this->_blockService->getBlock($microsite, $id_block);
+    public function getBlock(Request $request)
+    {
+
+        return $this->TryCatch(function () use ($request) {
+            $data = $this->_blockService->getBlock($request->route('microsite_id'), $request->route('block_id'));
             return $this->CreateJsonResponse(true, 201, 'messages.block_list', $data);
         });
 
     }
 
-	public function list($lang, $microsite, BlockListRequest $request){
-        return $this->TryCatch(function () use ($microsite, $request) {
-            $data = $this->_blockService->listado($microsite, $request->all());
-            return $this->CreateJsonResponse(true, 201, "messages.block_list",$data);
+    function list(BlockListRequest $request) {
+        return $this->TryCatch(function () use ($request) {
+            $data = $this->_blockService->listado($request->route('microsite_id'), $request->all());
+            return $this->CreateJsonResponse(true, 201, "messages.block_list", $data);
         });
-	
-	}
 
-
-    public function getTables($lang, $microsite, BlockListRequest $request){
-
-        return $this->TryCatch(function () use ($microsite, $request) {
-            $data = $this->_blockService->getTables($microsite, $request->all());
-            return $this->CreateJsonResponse(true, 201, "messages.block_list",$data);
-        });
-        
     }
-    
 
-	public function update($lang, $microsite, $block_id, BlockUpdateRequest $request){
+    public function getTables(BlockListRequest $request)
+    {
 
-		return $this->TryCatch(function () use ($microsite, $block_id, $request) {
-            $data = $this->_blockService->update($microsite, $block_id, $request->all());
+        return $this->TryCatch(function () use ($request) {
+            $data = $this->_blockService->getTables($request->route('microsite_id'), $request->all());
+            return $this->CreateJsonResponse(true, 201, "messages.block_list", $data);
+        });
+
+    }
+
+    public function update(BlockUpdateRequest $request)
+    {
+
+        return $this->TryCatch(function () use ($request) {
+            $data = $this->_blockService->update($request->route('microsite_id'), $request->route('block_id'), $request->all());
             return $this->CreateJsonResponse($data->estado, 201, trans($data->mensaje));
         });
-		
-	}
- 
+
+    }
+
 }
