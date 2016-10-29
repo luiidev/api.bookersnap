ba8e1004af096384395cb5c369b6f33932c3a494
Cesar Moreno <stonblizzard@gmail.com>
2016-10-29 14:43:07 -0500

    Se configuro busqueda de eventos por intervalos de horario de atencion del establecimiento

---
+++ app/Entities/res_turn_promotion.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class res_turn_promotion extends Model
+{
+    protected $table = "res_turn_promotion";
+
+    public function days()
+    {
+        return $this->hasMany('App\Entities\res_day_turn_promotion', 'res_turn_id');
+    }
+
+    public function event()
+    {
+        return $this->belongsTo('App\Entities\ev_event');
+    }
+}
