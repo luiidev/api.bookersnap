6e790dd31c26ff276ea363c9f6df282bdf512d3f
joper30 <joper30@gmail.com>
2016-11-05 11:05:56 -0500

    socket

--- app/Helpers/NotificationServerHelper.php
+++ app/Helpers/NotificationServerHelper.php
@@ -11,7 +11,8 @@ class NotificationServerHelper
 
     public function __construct()
     {
-        $this->_client = new Client(new Version1X('http://weblaravel.studework.com:1337'));
+        $url_socket    = config('settings.SOCKET_NOTIFICATION_URL');
+        $this->_client = new Client(new Version1X('http://127.0.0.1:1337'));
     }
     public function emit(string $emit, array $data)
     {
---
+++ config/settings.php
@@ -0,0 +1,6 @@
+<?php
+
+// para usar la constante usar: config('settings.NOMBRE_CONSTANTE')
+return [
+    'SOCKET_NOTIFICATION_URL' => 'http://127.0.0.1:1337',
+];
