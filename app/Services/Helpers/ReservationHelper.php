de5b4f891a48a72d2585997900275d0cf523e7e3
Josue Diaz Rimache <josue.diaz.dev@bookersnap.com>
2016-12-10 12:01:27 -0500

    filtros

--- app/Services/Helpers/ReservationHelper.php
+++ app/Services/Helpers/ReservationHelper.php
@@ -10,29 +10,43 @@ class ReservationHelper
     //En la url no poner el campo a filtrar si no un alias, devuelve el campo real
     public function getNameSort(string $value)
     {
-        $sort = "";
-        switch ($value) {
+        $value        = $this->validateOrderBy($value);
+        $sort         = [];
+        $order        = explode(".", $value);
+        $sort['type'] = $order[1];
+
+        switch ($order[0]) {
             case 'time':
-                $sort = 'hours_reservation';
+                $sort['value'] = 'hours_reservation';
                 break;
             case 'status':
-                $sort = 'res_reservation_status_id';
+                $sort['value'] = 'res_reservation_status_id';
                 break;
             case 'covers':
-                $sort = 'num_guest';
+                $sort['value'] = 'num_guest';
                 break;
             case 'guest':
-                $sort = 'guest.first_name';
+                $sort['value'] = 'guest.first_name';
                 break;
             case 'table':
-                $sort = 'table.name';
+                $sort['value'] = 'table.name';
                 break;
-
             default:
-                # code...
+                $sort['value'] = 'hours_reservation';
                 break;
         }
-        return $sort;
+        return (object) $sort;
+    }
+
+    private function validateOrderBy(string $value)
+    {
+        $pos = strpos($value, ".");
+
+        if ($pos === false) {
+            $value = $value . ".asc";
+        }
+
+        return $value;
     }
 
 }
